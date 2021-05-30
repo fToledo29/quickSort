82cd032d7de4066913a2ec00e1ed6c5fc3bd5959
Fernando Toledo <>
2021-05-30 23:05:46 +0000

    Adding some updates

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
+const performance = require('perf_hooks').performance;
+
 const quickSort = (array) => {
+
    if (array.length <= 1) {
         return array;
     }
     
     let i = 0;
-    let rigth = [];
+
+    let right = [];
+
     let left = [];
+
     const pivot = array[array.length - 1];
-    const n = array.slice(0, array.length - 1).length;
+
+    const n = array.length - 1;
 
     while (i < n) {
-        array[i] < pivot ? left.push(array[i++]) : rigth.push(array[i++]);
+        array[i] < pivot ? left.push(array[i++]) : right.push(array[i++]);
     }
     
-    return [...quickSort(left), pivot, ...quickSort(rigth)];
+    return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
-const arrInput = [564345.12, 43, 32.5, 3, 52, 2399, 89, 12, 329, 51, 46, 10];
-
-console.log('Input: ', arrInput);
+const arrInput = [
+	564345.12, 43, 32.5, 3, 52, 
+	0.23, 2399, 89, 12, 329, 51, 46, 10];
 
 console.log('Length: ', arrInput.length);
 
-console.log('Output: ', quickSort(arrInput));
+console.log('Input: ', arrInput);
+
+const t0 = performance.now();
+
+const res = quickSort(arrInput);
+
+const t1 = performance.now();
+
+console.log(`quickSort call took: ` + (t1 - t0) + " milliseconds.")
+
+console.log('Output: ', res);
